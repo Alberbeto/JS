1e897d5a76e3dc5fef53e8b5ed4afd5008de566d
Alberto Medina <betho.medina.encalada@gmail.com>
2024-11-22 15:53:14 -0500

    declaracion de variables

---
+++ CONDICIONALES/ejercicio1.js
@@ -0,0 +1,16 @@
+/*
+Declara una variable a,b,c con valñores numericos y utiliza
+condiciones para deternimar cual de ellos es el mayot
+*/
+
+const a=10;
+const b=25;
+const c= 45;
+
+if(a>b&& a>c){
+    console.log('a es el mayor');
+}else if(b>a&& b>c){
+    console.log('b es el mayor');
+}else {
+    console.log('c es el mayor');
+}
